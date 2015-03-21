80f30992483b9ad398cf5d1adb54a8a739131b39
thiagobloomfield <thiagobloomf@gmail.com>
2015-03-21 17:15:37 -0300

    banco atualizado

--- database/banco.sql
+++ database/banco.sql
@@ -1,194 +1,301 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+CREATE DATABASE  IF NOT EXISTS `focanopreco` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `focanopreco`;
+-- MySQL dump 10.13  Distrib 5.1.40, for Win32 (ia32)
+--
+-- Host: localhost    Database: focanopreco
+-- ------------------------------------------------------
+-- Server version	5.1.59-community
 
-CREATE SCHEMA IF NOT EXISTS `focanopreco` DEFAULT CHARACTER SET utf8 ;
-USE `focanopreco` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `focanopreco`.`perfil`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `focanopreco`.`perfil` (
-  `cod_perfil` INT NOT NULL AUTO_INCREMENT ,
-  `nom_perfil` VARCHAR(45) NOT NULL ,
-  `des_perfil` VARCHAR(45) NULL ,
-  PRIMARY KEY (`cod_perfil`) )
-ENGINE = InnoDB;
+--
+-- Table structure for table `sub_categoria`
+--
 
+DROP TABLE IF EXISTS `sub_categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sub_categoria` (
+  `cod_sub_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_sub_categoria` varchar(45) NOT NULL,
+  `des_sub_categoria` varchar(45) DEFAULT NULL,
+  `cod_categoria` int(11) NOT NULL,
+  PRIMARY KEY (`cod_sub_categoria`),
+  KEY `fk_sub_categoria_categora1_idx` (`cod_categoria`),
+  CONSTRAINT `fk_sub_categoria_categora1` FOREIGN KEY (`cod_categoria`) REFERENCES `categora` (`cod_categoria`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `focanopreco`.`usuario`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `focanopreco`.`usuario` (
-  `cod_usuario` INT NOT NULL AUTO_INCREMENT ,
-  `nom_usuario` VARCHAR(45) NOT NULL ,
-  `sob_nom_usuario` VARCHAR(45) NULL ,
-  `ind_usuario` INT NOT NULL DEFAULT 0 ,
-  `cod_perfil` INT NOT NULL ,
-  `cod_usu_pai` INT NULL ,
-  `co_usuario_pai` INT NULL ,
-  PRIMARY KEY (`cod_usuario`) ,
-  INDEX `fk_usuario_perfil_idx` (`cod_perfil` ASC) ,
-  INDEX `fk_usuario_usuario1` (`co_usuario_pai` ASC) ,
-  CONSTRAINT `fk_usuario_perfil`
-    FOREIGN KEY (`cod_perfil` )
-    REFERENCES `focanopreco`.`perfil` (`cod_perfil` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_usuario_usuario1`
-    FOREIGN KEY (`co_usuario_pai` )
-    REFERENCES `focanopreco`.`usuario` (`cod_usuario` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `sub_categoria`
+--
 
+LOCK TABLES `sub_categoria` WRITE;
+/*!40000 ALTER TABLE `sub_categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sub_categoria` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `focanopreco`.`categora`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `focanopreco`.`categora` (
-  `cod_categoria` INT NOT NULL AUTO_INCREMENT ,
-  `nom_categora` VARCHAR(45) NOT NULL ,
-  `des_categoria` VARCHAR(45) NULL ,
-  PRIMARY KEY (`cod_categoria`) )
-ENGINE = InnoDB;
+--
+-- Table structure for table `pessoa_juridica`
+--
 
+DROP TABLE IF EXISTS `pessoa_juridica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pessoa_juridica` (
+  `cod_pessoa_juridica` int(11) NOT NULL AUTO_INCREMENT,
+  `num_cnpj` int(11) NOT NULL,
+  `cod_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`cod_pessoa_juridica`),
+  KEY `fk_pessoa_juridica_usuario1_idx` (`cod_usuario`),
+  CONSTRAINT `fk_pessoa_juridica_usuario1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `focanopreco`.`produto`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `focanopreco`.`produto` (
-  `cod_produto` INT NOT NULL AUTO_INCREMENT ,
-  `nom_produto` VARCHAR(45) NULL ,
-  `des_produto` VARCHAR(45) NULL ,
-  `cod_usuario` INT NOT NULL ,
-  `cod_categoria` INT NOT NULL ,
-  `pre_produto` DECIMAL(10,2) NULL ,
-  PRIMARY KEY (`cod_produto`) ,
-  INDEX `fk_produto_usuario1_idx` (`cod_usuario` ASC) ,
-  INDEX `fk_produto_categora1_idx` (`cod_categoria` ASC) ,
-  CONSTRAINT `fk_produto_usuario1`
-    FOREIGN KEY (`cod_usuario` )
-    REFERENCES `focanopreco`.`usuario` (`cod_usuario` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_produto_categora1`
-    FOREIGN KEY (`cod_categoria` )
-    REFERENCES `focanopreco`.`categora` (`cod_categoria` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `pessoa_juridica`
+--
 
+LOCK TABLES `pessoa_juridica` WRITE;
+/*!40000 ALTER TABLE `pessoa_juridica` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pessoa_juridica` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `focanopreco`.`pessoa_juridica`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `focanopreco`.`pessoa_juridica` (
-  `cod_pessoa_juridica` INT NOT NULL AUTO_INCREMENT ,
-  `num_cnpj` INT NOT NULL ,
-  `cod_usuario` INT NOT NULL ,
-  PRIMARY KEY (`cod_pessoa_juridica`) ,
-  INDEX `fk_pessoa_juridica_usuario1_idx` (`cod_usuario` ASC) ,
-  CONSTRAINT `fk_pessoa_juridica_usuario1`
-    FOREIGN KEY (`cod_usuario` )
-    REFERENCES `focanopreco`.`usuario` (`cod_usuario` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+--
+-- Table structure for table `usuario`
+--
 
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `cod_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_usuario` varchar(45) NOT NULL,
+  `sob_nom_usuario` varchar(45) DEFAULT NULL,
+  `ind_usuario` int(11) NOT NULL DEFAULT '0',
+  `cod_perfil` int(11) NOT NULL,
+  `co_usuario_pai` int(11) DEFAULT NULL,
+  `nom_login` varchar(45) NOT NULL,
+  `des_senha` varchar(45) NOT NULL,
+  PRIMARY KEY (`cod_usuario`),
+  KEY `fk_usuario_perfil_idx` (`cod_perfil`),
+  KEY `fk_usuario_usuario1` (`co_usuario_pai`),
+  CONSTRAINT `fk_usuario_perfil` FOREIGN KEY (`cod_perfil`) REFERENCES `perfil` (`cod_perfil`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_usuario_usuario1` FOREIGN KEY (`co_usuario_pai`) REFERENCES `usuario` (`cod_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `focanopreco`.`pessoa_fisica`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `focanopreco`.`pessoa_fisica` (
-  `cod_pessoa_fisica` INT NOT NULL AUTO_INCREMENT ,
-  `num_cpf` INT NOT NULL ,
-  `cod_usuario` INT NOT NULL ,
-  PRIMARY KEY (`cod_pessoa_fisica`) ,
-  INDEX `fk_pessoa_fisica_usuario1_idx` (`cod_usuario` ASC) ,
-  CONSTRAINT `fk_pessoa_fisica_usuario1`
-    FOREIGN KEY (`cod_usuario` )
-    REFERENCES `focanopreco`.`usuario` (`cod_usuario` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `usuario`
+--
 
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Administrador','Admin',1,1,NULL,'admin','admin');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `focanopreco`.`sub_categoria`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `focanopreco`.`sub_categoria` (
-  `cod_sub_categoria` INT NOT NULL AUTO_INCREMENT ,
-  `nom_sub_categoria` VARCHAR(45) NOT NULL ,
-  `des_sub_categoria` VARCHAR(45) NULL ,
-  `cod_categoria` INT NOT NULL ,
-  PRIMARY KEY (`cod_sub_categoria`) ,
-  INDEX `fk_sub_categoria_categora1_idx` (`cod_categoria` ASC) ,
-  CONSTRAINT `fk_sub_categoria_categora1`
-    FOREIGN KEY (`cod_categoria` )
-    REFERENCES `focanopreco`.`categora` (`cod_categoria` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+--
+-- Table structure for table `produto`
+--
 
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produto` (
+  `cod_produto` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_produto` varchar(45) DEFAULT NULL,
+  `des_produto` varchar(45) DEFAULT NULL,
+  `cod_usuario` int(11) NOT NULL,
+  `cod_categoria` int(11) NOT NULL,
+  `pre_produto` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`cod_produto`),
+  KEY `fk_produto_usuario1_idx` (`cod_usuario`),
+  KEY `fk_produto_categora1_idx` (`cod_categoria`),
+  CONSTRAINT `fk_produto_usuario1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_produto_categora1` FOREIGN KEY (`cod_categoria`) REFERENCES `categora` (`cod_categoria`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `focanopreco`.`avaliacao`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `focanopreco`.`avaliacao` (
-  `cod_avaliacao` INT NOT NULL ,
-  `des_avaliacao` TEXT NOT NULL ,
-  `cod_tipo_avalicao` INT NOT NULL ,
-  `ind_status` INT NOT NULL ,
-  PRIMARY KEY (`cod_avaliacao`) )
-ENGINE = InnoDB;
+--
+-- Dumping data for table `produto`
+--
 
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `focanopreco`.`avaliacao_produto`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `focanopreco`.`avaliacao_produto` (
-  `cod_avaliacao` INT NOT NULL ,
-  `cod_produto` INT NOT NULL ,
-  INDEX `fk_avaliacao_produto_avaliacao1_idx` (`cod_avaliacao` ASC) ,
-  INDEX `fk_avaliacao_produto_produto1_idx` (`cod_produto` ASC) ,
-  PRIMARY KEY (`cod_avaliacao`, `cod_produto`) ,
-  CONSTRAINT `fk_avaliacao_produto_avaliacao1`
-    FOREIGN KEY (`cod_avaliacao` )
-    REFERENCES `focanopreco`.`avaliacao` (`cod_avaliacao` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_avaliacao_produto_produto1`
-    FOREIGN KEY (`cod_produto` )
-    REFERENCES `focanopreco`.`produto` (`cod_produto` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+--
+-- Table structure for table `avaliacao_produto`
+--
 
+DROP TABLE IF EXISTS `avaliacao_produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `avaliacao_produto` (
+  `cod_avaliacao` int(11) NOT NULL,
+  `cod_produto` int(11) NOT NULL,
+  PRIMARY KEY (`cod_avaliacao`,`cod_produto`),
+  KEY `fk_avaliacao_produto_avaliacao1_idx` (`cod_avaliacao`),
+  KEY `fk_avaliacao_produto_produto1_idx` (`cod_produto`),
+  CONSTRAINT `fk_avaliacao_produto_avaliacao1` FOREIGN KEY (`cod_avaliacao`) REFERENCES `avaliacao` (`cod_avaliacao`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_avaliacao_produto_produto1` FOREIGN KEY (`cod_produto`) REFERENCES `produto` (`cod_produto`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `focanopreco`.`avalicao_usuario`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `focanopreco`.`avalicao_usuario` (
-  `cod_avaliacao` INT NOT NULL ,
-  `cod_usuario` INT NULL ,
-  INDEX `fk_avalicao_usuario_avaliacao1_idx` (`cod_avaliacao` ASC) ,
-  INDEX `fk_avalicao_usuario_usuario1_idx` (`cod_usuario` ASC) ,
-  PRIMARY KEY (`cod_avaliacao`) ,
-  CONSTRAINT `fk_avalicao_usuario_avaliacao1`
-    FOREIGN KEY (`cod_avaliacao` )
-    REFERENCES `focanopreco`.`avaliacao` (`cod_avaliacao` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_avalicao_usuario_usuario1`
-    FOREIGN KEY (`cod_usuario` )
-    REFERENCES `focanopreco`.`usuario` (`cod_usuario` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `avaliacao_produto`
+--
 
+LOCK TABLES `avaliacao_produto` WRITE;
+/*!40000 ALTER TABLE `avaliacao_produto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `avaliacao_produto` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `perfil`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `perfil`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `perfil` (
+  `cod_perfil` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_perfil` varchar(45) NOT NULL,
+  `des_perfil` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`cod_perfil`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `perfil`
+--
+
+LOCK TABLES `perfil` WRITE;
+/*!40000 ALTER TABLE `perfil` DISABLE KEYS */;
+INSERT INTO `perfil` VALUES (1,'ADMINISTRADOR','ADMINISTRADOR SISTEMA'),(2,'ANUNCIANTE','ANUNCIANTE DE PRODUTO');
+/*!40000 ALTER TABLE `perfil` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `avalicao_usuario`
+--
+
+DROP TABLE IF EXISTS `avalicao_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `avalicao_usuario` (
+  `cod_avaliacao` int(11) NOT NULL,
+  `cod_usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`cod_avaliacao`),
+  KEY `fk_avalicao_usuario_avaliacao1_idx` (`cod_avaliacao`),
+  KEY `fk_avalicao_usuario_usuario1_idx` (`cod_usuario`),
+  CONSTRAINT `fk_avalicao_usuario_avaliacao1` FOREIGN KEY (`cod_avaliacao`) REFERENCES `avaliacao` (`cod_avaliacao`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_avalicao_usuario_usuario1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `avalicao_usuario`
+--
+
+LOCK TABLES `avalicao_usuario` WRITE;
+/*!40000 ALTER TABLE `avalicao_usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `avalicao_usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categora`
+--
+
+DROP TABLE IF EXISTS `categora`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categora` (
+  `cod_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_categora` varchar(45) NOT NULL,
+  `des_categoria` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`cod_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categora`
+--
+
+LOCK TABLES `categora` WRITE;
+/*!40000 ALTER TABLE `categora` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categora` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pessoa_fisica`
+--
+
+DROP TABLE IF EXISTS `pessoa_fisica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pessoa_fisica` (
+  `cod_pessoa_fisica` int(11) NOT NULL AUTO_INCREMENT,
+  `num_cpf` int(11) NOT NULL,
+  `cod_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`cod_pessoa_fisica`),
+  KEY `fk_pessoa_fisica_usuario1_idx` (`cod_usuario`),
+  CONSTRAINT `fk_pessoa_fisica_usuario1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoa_fisica`
+--
+
+LOCK TABLES `pessoa_fisica` WRITE;
+/*!40000 ALTER TABLE `pessoa_fisica` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pessoa_fisica` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `avaliacao`
+--
+
+DROP TABLE IF EXISTS `avaliacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `avaliacao` (
+  `cod_avaliacao` int(11) NOT NULL,
+  `des_avaliacao` text NOT NULL,
+  `cod_tipo_avalicao` int(11) NOT NULL,
+  `ind_status` int(11) NOT NULL,
+  PRIMARY KEY (`cod_avaliacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `avaliacao`
+--
+
+LOCK TABLES `avaliacao` WRITE;
+/*!40000 ALTER TABLE `avaliacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `avaliacao` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-03-21 17:15:25
