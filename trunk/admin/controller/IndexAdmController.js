13727c911afab5a5a5886dddf4ee1bb502d75fc4
thiagobloomfield <thiagobloomf@gmail.com>
2015-03-22 14:52:07 -0300

    Produtos

--- trunk/admin/controller/IndexAdmController.js
+++ trunk/admin/controller/IndexAdmController.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
     
-    $("#superMenu").load("../includes/SuperMenu.html");
+    $("#menu").load("../includes/Menu.html", function(){
+        $("#superMenu").load("../includes/SuperMenu.html", function(){
+            $("#pleaseWait").hide();
+        });
+    });
+
+    listaQtdProduto();
     
-    $("#menu").load("../includes/Menu.html");
+    function listaQtdProduto(){
+        $.post( "../service/IndexService.php", {
+                action:'listar_produto_usuario'
+            },
+            function( data ) {
+                if(data["codErro"] == 0){
+                    $("#qtdProduto").html(data["countProduto"]);
+                }else{
+                    window.location = "pages/login.html";
+                }
+            }
+        ,"json");
+    }
 });
