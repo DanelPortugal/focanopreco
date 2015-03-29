617bb727a0b045a840170e5ee917da948ee747e5
thiagobloomfield <thiagobloomf@gmail.com>
2015-03-29 20:11:00 -0300

    login no menu

--- trunk/controller/IndexController.js
+++ trunk/controller/IndexController.js
@@ -22,6 +22,11 @@ $(document).ready(function(){
         window.open("view/detalheproduto.html?produto=" + $(this).attr("data_cod_produto"), "_self");
     });
     
+    $(document).on('click', '#divLoginIndex', function (event) {
+        $(this).parent().addClass('open');
+        event.stopPropagation();
+    });
+    
     function listaMenu(){
         $.post( "service/MenuService.php", {
                 action:'lista_menu_index'
@@ -69,3 +74,25 @@ $(document).ready(function(){
         ,"json");
     }
 });
+
+//LOGIN GOOGLE
+
+(function() {
+       var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+       po.src = 'https://apis.google.com/js/client:plusone.js';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+     
+function signinCallback(authResult) {
+    if (authResult['access_token']) {
+    // Autorizado com sucesso
+    // Ocultar o botão de login agora que o usuário está autorizado, por exemplo:
+    //document.getElementById('signinButton').setAttribute('style', 'display: none');
+    } else if (authResult['error']) {
+    // Ocorreu um erro.
+    // Possíveis códigos de erro:
+    //  "access_denied" - o usuário negou o acesso a seu aplicativo
+    //   "immediate_failed" - não foi possível fazer o login do usuário automaticamente
+    // console.log('There was an error: ' + authResult['error']);
+    }
+}
--- trunk/service/MenuService.php
+++ trunk/service/MenuService.php
@@ -20,58 +20,78 @@
     function retornaHtml(){
         $htmlProd = '<nav class="navbar navbar-default">
                         <div class="container-fluid">
-                            <!-- Brand and toggle get grouped for better mobile display -->
                             <div class="navbar-header">
-                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                                    <span class="sr-only">Toggle navigation</span>
-                                    <span class="icon-bar"></span>
-                                    <span class="icon-bar"></span>
-                                    <span class="icon-bar"></span>
-                                </button>
+                              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                              </button>
                             </div>
-                        
-                            <!-- Collect the nav links, forms, and other content for toggling -->
                             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                                        
-                                <ul class="nav navbar-nav navbar-left>
-                                    <li class="dropdown">
-                                        <a href="#" class="btn dropdown-toggle fa fa-user fa-2x" data-toggle="dropdown" role="button" aria-expanded="false"><span class="caret fa-2x"></span></a>
+                              <ul class="nav navbar-nav">
+                                    <li class="dropdown pull-left">
+                                        <a href="#" class="btn dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true">
+                                            <span class="glyphicon glyphicon-align-justify fa-2x"></span>
+                                            <span>Cidades</span>
+                                        </a>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li>
+                                                <a href="#">Rio de Janeiro</a>
+                                            </li>
+                                        </ul>
                                     </li>
-                                </ul>
-                                    
-                                <ul class="nav navbar-nav navbar-right">
-                                    <li class="dropdown">';
+                              </ul>
+                                <ul class="nav nav-pills navbar-right">
+                                    <li class="dropdown pull-left">';
         if(isset($_SESSION["logged"])){
-            $htmlProd .= '<span>Bem vindo: '.$_SESSION["nome_usuario"].'</span>';
+            $htmlProd .= '              <span>Bem vindo: '.$_SESSION["nome_usuario"].'</span>';
         }else{
             $htmlProd .= ' 
-                            <a href="#" class="btn dropdown-toggle fa fa-user fa-2x" data-toggle="dropdown" role="button" aria-expanded="false"><span class="caret fa-2x"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li class="col-md-12">
-                                    <br/>
-                                    <form role="form">
-                                        <fieldset>
-                                            <div class="form-group">
-                                                <input class="form-control" type="text" id="txtLogin" placeholder="Login" autofocus>
+                                        <a href="#" class="btn dropdown-toggle fa fa-user fa-2x" data-toggle="dropdown" role="button" aria-expanded="false">
+                                            <span class="caret fa-2x"></span>
+                                        </a>
+                                        <div class="dropdown-menu" role="menu" id="divLoginIndex">
+                                            <div class="col-md-12">
+                                                <br/>
+                                                <div class="form-group">
+                                                    <input class="form-control" type="text" id="txtLogin" placeholder="Login" autofocus>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input class="form-control" type="password" placeholder="Senha" id="txtSenha">
+                                                </div>
+                                                <a id="btnLogin" class="btn btn-lg btn-success btn-block">Entrar</a>
                                             </div>
-                                            <div class="form-group">
-                                                <input class="form-control" type="password" placeholder="Senha" id="txtSenha">
+                                            <div class="col-md-12">
+                                                <br/>
+                                                <b>Cadastre-se</b> <a href="#" class="btn">Aqui</a>
                                             </div>
-                                            <!-- Change this to a button or input when using this as a form -->
-                                            <a id="btnLogin" class="btn btn-lg btn-success btn-block">Entrar</a>
-                                        </fieldset>
-                                    </form>
-                                </li>
-                                <li class="col-md-12">
-                                    <br/>
-                                    <a class="btn btn-block btn-social btn-facebook" href="https://www.facebook.com/dialog/oauth?client_id=898030020238477&redirect_uri=http://bloomfieldserver.no-ip.info/focanopreco/trunk/social_network/fbLogin.php&scope=email,user_website,user_location">
-                                        <i class="fa fa-facebook"></i><span class="pull-right" style="padding-left: 25px;">Login Facebook</span>
-                                    </a>
-                                    <br/>
-                                </li>
-                            </ul>';
+                                            <div class="col-md-12">
+                                                <br/>
+                                                <a class="btn btn-block btn-social btn-facebook" href="https://www.facebook.com/dialog/oauth?client_id=898030020238477&redirect_uri=http://bloomfieldserver.no-ip.info/focanopreco/trunk/social_network/fbLogin.php&scope=email,user_website,user_location">
+                                                    <i class="fa fa-facebook"></i><span class="pull-right" style="padding-left: 25px;">Login Facebook</span>
+                                                </a>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <br/>
+                                                <span id="signinButton">
+                                                  <span
+                                                    class="g-signin"
+                                                    data-callback="signinCallback"
+                                                    data-clientid="63476639202-gk0k9atfgo8p2hoqlo1ocd1d5n3t8b83.apps.googleusercontent.com"
+                                                    data-cookiepolicy="single_host_origin"
+                                                    data-requestvisibleactions="http://schemas.google.com/AddActivity"
+                                                    data-scope="https://www.googleapis.com/auth/plus.login">
+                                                  </span>
+                                                </span>
+                                            </div>
+                                        </div>';
         }
-        $htmlProd .= '</li></ul></div></div></nav>';
+        $htmlProd .= '              </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>';
         return $htmlProd;
     }
 ?>
