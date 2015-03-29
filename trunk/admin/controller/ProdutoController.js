9c9050893342a5940d603b93981c7200b1b27420
thiagobloomfield <thiagobloomf@gmail.com>
2015-03-29 02:30:56 -0300

    destroy

--- trunk/admin/controller/ProdutoController.js
+++ trunk/admin/controller/ProdutoController.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
         });
     });
     
+    var table = null;
+    
     $("#preProdutoInsert").maskMoney();
     
     $(document).off("click", "#btnInserirProd");
@@ -52,13 +54,30 @@ $(document).ready(function(){
                 if(data["codErro"] == 0){
                     $("#tbdProduto").html(data["html"]);
                     
-                    if($("#tbdProduto").html() != ""){
-                        $('#tblProduto').addClass('no-wrap');
-
-                        $('#tblProduto').DataTable( {
-                            responsive: true
-                        } );
+                    if(table != null){
+                        table.destroy();
                     }
+                    
+                    $('#tblProduto').addClass('no-wrap');
+
+                    $('#tblProduto').DataTable( {
+                        "oLanguage": {
+                            "sProcessing": "Aguarde enquanto os dados são carregados ...",
+                            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+                            "sZeroRecords": "Nenhum registro correspondente ao criterio encontrado",
+                            "sInfoEmtpy": "Exibindo 0 a 0 de 0 registros",
+                            "sInfo": "Exibindo de _START_ a _END_ de _TOTAL_ registros",
+                            "sInfoFiltered": "",
+                            "sSearch": "Procurar",
+                                "oPaginate": {
+                                "sFirst":    "Primeiro",
+                                "sPrevious": "Anterior",
+                                "sNext":     "Pr&oacute;ximo",
+                                "sLast":     "&Uacute;ltimo"
+                            }
+                        },      
+                        responsive: true
+                    } );                                        
                     
                 }else{
                     window.location = "pages/login.html";
